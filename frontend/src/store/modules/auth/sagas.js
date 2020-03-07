8f74c1dd44d4b27007e39bac9ed58f6b8b3ff4dc
elvinciqueira <elvinciqueira@gmail.com>
2020-03-07 16:57:33 -0300

    feat: exibindo toasts

--- frontend/src/store/modules/auth/sagas.js
+++ frontend/src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { all, takeLatest, call, put } from "redux-saga/effects";
+import { toast } from "react-toastify";
 import { signInSuccess, signFailure } from "./actions";
 import api from "~/services/api";
 import history from "~/services/history";
@@ -15,7 +16,7 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
-      console.tron.error("Usuário não é prestador");
+      toast.error("Usuário não é prestador");
 
       return;
     }
@@ -24,6 +25,8 @@ export function* signIn({ payload }) {
 
     history.push("/dashboard");
   } catch (error) {
+    toast.error("Falha na autentificação, verifique seus dados");
+
     yield put(signFailure());
   }
 }
